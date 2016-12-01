457d7b9bf6ee68f2e928ce1708910099efd41c5f
Marie <marie.b@codeur.online>
2016-12-01 16:09:54 +0100

    correction

--- PHPaffiche.php
+++ PHPaffiche.php
@@ -2,7 +2,7 @@
 
 include'.init.php';
 
-$sth = $conn->prepare('SELECT BLOG_ARTICLE.Title, BLOG_ARTICLE.Content, BLOG_ARTICLE.Date, BLOG_CATEGORY.Category
+$sth = $conn->prepare('SELECT BLOG_ARTICLE.Title, BLOG_ARTICLE.Content, BLOG_ARTICLE.Date, BLOG_ARTICLE.Author_ID, BLOG_CATEGORY.Category
 FROM BLOG_ARTICLE
 NATURAL JOIN BLOG_CATEGORY
 ');
@@ -17,13 +17,14 @@ print_r($result);
 
 $template = file_get_contents("blockArticle.html");
 
-for (i=0; i<$result[0].lenght; i++)
+for (i=0; i<$result.lenght; i++)
 
 	{
-		$tpl = str_replace("{{TITRE}}", $result[i][Article_ID]);
-    	$tpl = str_replace("{{Author}}", $result[i][Author_ID]);
-    	$tpl = str_replace("{{Date}}", $result[i][Author_ID]);
-    	$tpl = str_replace("{{Category}}", $result[i][Category_ID]);
+		$tpl = str_replace("{{id_article}}", $result[i][Article_ID],$template);
+		$tpl = str_replace("{{Titre}}", $result[i][Title],$tpl);
+    	$tpl = str_replace("{{Author}}", $result[i][Author_ID],$tpl);
+    	$tpl = str_replace("{{Date}}", $result[i][Date],$tpl);
+    	$tpl = str_replace("{{Category}}", $result[i][Category],$tpl);
     	
     }
 
