0129c8c6de2416f969321984725ab4b1eee262fc
YetAnotherBen <benoit.m@codeur.online>
2016-12-01 16:40:14 +0100

    Slight changes
    indentation only

--- blog_form.php
+++ blog_form.php
@@ -1,13 +1,18 @@
 <?php
 include '.init.php';
 
+//TABLES TO FILL WITH DATA
+$tableAuthor = "BLOG_AUTHOR";
+$tableArticle = "BLOG_ARTICLE";
+$tableCategory = "BLOG_CATEGORY";
+
 
 //DATA TO GATHER FROM FORM
 $formName = $_REQUEST['form_author'];
 $formTitle = $_REQUEST['form_title'];
 $formContent = $_REQUEST['form_content'];
-
-if (isset($_REQUEST['form_category']))
+//IF THE CATEGORY RADIO IS NOT SET, THEN IT WILL BE 'UNDEFINED'
+if (isset($_REQUEST['form_category'])) 
     {
         $formCategory = $_REQUEST['form_category'];
     }
@@ -17,22 +22,18 @@ else
     }
 
 
-//TABLES TO FILL WITH DATA
-$tableAuthor = "BLOG_AUTHOR";
-$tableArticle = "BLOG_ARTICLE";
-$tableCategory = "BLOG_CATEGORY";
 
-
-//NAME REQUEST
+//NAME REQUEST => IF THE NAME ALREADY EXIST THEN IT'S NOT ADDING A NEW ONE
 try
     {
         $sth = $conn->prepare('INSERT INTO `'.$tableAuthor.'` (Name) VALUES (:Name)');
         $sth -> bindParam(':Name', $formName);
         $sth -> execute();
     }
-catch(Exception $e) {
-  echo 'Message: ' .$e->getMessage();
-}
+catch(Exception $e) 
+    {
+        /*echo 'Message: ' .$e->getMessage();*/
+    }
 
 
 //AUTHOR_ID REQUEST - IN ORDER TO LINK BLOG_ARTICLE AND BLOG_AUTHOR TABLES
@@ -49,13 +50,12 @@ $sth -> execute();
 $idCategory = $sth->fetchAll();
 
 
-//TITLE & CONTENT REQUEST (WILL ADD CATEGORY REQUEST TO IT ... SOON)
+//TITLE & CONTENT & CATEGORY REQUEST 
 $sth = $conn->prepare('INSERT INTO `'.$tableArticle.'` (Title, Content, Author_ID, Category_ID) VALUES (:Title, :Content, :AuthorID, :CategoryID)');
 $sth -> bindParam(':CategoryID', $idCategory[0]['Category_ID']);
 $sth -> bindParam(':AuthorID', $idAuthor[0]['Author_ID']);
 $sth -> bindParam(':Title', $formTitle);
 $sth -> bindParam(':Content', $formContent);
 $sth -> execute();
-
 
 ?>
--- import_category.php
+++ import_category.php
@@ -1,7 +1,5 @@
 <?php
 include '.init.php';
-$templateCategory = file_get_contents("blockcategory.html");
-
 
 //TABLES TO FILL WITH DATA
 $tableCategory = "BLOG_CATEGORY";
@@ -12,12 +10,10 @@ $sth = $conn->prepare('SELECT Category FROM `'.$tableCategory.'`');
 $sth->execute();
 $result = $sth->fetchAll();
 
-/*print_r($result);*/
-
 foreach ($result as $value)
 	{
+		$templateCategory = file_get_contents("blockcategory.html");
 		$tpld = str_replace("{{nomCategory}}", $value[0], $templateCategory);
-      	
       	echo $tpld;
 	}
 
