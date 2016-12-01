9208535563e79f88550e32563443de7eb33da408
Marie <marie.b@codeur.online>
2016-12-01 16:16:25 +0100

    correction

--- PHPaffiche.php
+++ PHPaffiche.php
@@ -17,14 +17,14 @@ print_r($result);
 
 $template = file_get_contents("blockArticle.html");
 
-for (i=0; i<$result.lenght; i++)
+for ($i=0; $i<$result.lenght; $i++)
 
 	{
-		$tpl = str_replace("{{id_article}}", $result[i][Article_ID],$template);
-		$tpl = str_replace("{{Titre}}", $result[i][Title],$tpl);
-    	$tpl = str_replace("{{Author}}", $result[i][Author_ID],$tpl);
-    	$tpl = str_replace("{{Date}}", $result[i][Date],$tpl);
-    	$tpl = str_replace("{{Category}}", $result[i][Category],$tpl);
+		$tpl = str_replace("{{id_article}}", $result[$i]['Article_ID'],$template);
+		$tpl = str_replace("{{Titre}}", $result[$i]['Title'],$tpl);
+    	$tpl = str_replace("{{Author}}", $result[$i]['Author_ID'],$tpl);
+    	$tpl = str_replace("{{Date}}", $result[$i]['Date'],$tpl);
+    	$tpl = str_replace("{{Category}}", $result[$i]['Category'],$tpl);
     	
     }
 
